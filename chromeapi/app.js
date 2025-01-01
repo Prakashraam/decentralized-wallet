8314ba9f63bd36120a7c24dd09c554c04f60091a
Prakashraam <prakashraam@student.tce.edu>
2025-01-01 13:07:01 +0530

    first commit

--- chromeapi/app.js
+++ chromeapi/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const userRouter = require("./Api/Routers/userRouter");
 const tokenRouter = require("./Api/Routers/tokenRouter"); const accountRouter = require("./Api/Routers/accountRouter");
-//MIDDLEWARE
+
 const app = express();
 app.use(express.json({ limit: "100kb"}));
 app.use(cors())
